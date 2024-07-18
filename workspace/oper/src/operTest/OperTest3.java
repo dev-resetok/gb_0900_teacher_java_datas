2ee5f966ae5a034155ec1665f2373367e15b10c4
seungmin <746aa1357@gmail.com>
2024-07-18 14:20:46 +0900

    day08

---
+++ workspace/oper/src/operTest/OperTest3.java
@@ -0,0 +1,12 @@
+package operTest;
+
+public class OperTest3 {
+	public static void main(String[] args) {
+//		사용자에게 키를 입력받고 
+//		정수라면 정수로 출력
+//		실수라면 실수로 출력
+//		예)	183 -> 183 출력
+//		   	183.5 -> 183.5 출력
+//			183.0 -> 183 출력
+	}
+}
